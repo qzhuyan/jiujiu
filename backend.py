057123fcc649b113bd469b8dcb1d86522d686628
unknown <Administrator@.(none)>
2011-01-08 23:20:22 +0800

    Deliver for test 	modified:   backend.py 	new file:   book1.xls

--- backend.py
+++ backend.py
@@ -2,14 +2,35 @@
 #!/usr/bin/env python
 
 import datetime;
+import xlrd
+from xlutils.copy import copy
+from xlrd import open_workbook
+
 
 class RecordClient():
     AskUserIdString = "请输入员工号\n"
-    UserDict = dict(a1="周星驰",a2="梁朝伟",a3="猪八戒",a4="孙悟空")
-    ShiftDict = dict(Morning="早班",Middle="中班",Night="晚班")
-    MPdict = dict(a777888=("机器1","脸盆"),a999000=("机器2","牙膏"))
-    ParmDict = {"aa":0,"bb":0,"cc":0,"ee":0}
     
+    #这里定义员工号和员工姓名 a1是员工号，周星驰是员工姓名
+    UserDict = dict(a1="周星驰",a2="梁朝伟",a3="猪八戒",a4="孙悟空")
+
+    #这里定义几个班，早中晚
+    ShiftDict = dict(Morning="早班",Middle="中班",Night="晚班")
+
+    #这里定义条形码组合
+    MPdict = dict(a777888=("机器1","脸盆"),a999000=("机器2","牙膏"))
+
+    #这里定义要输入的参数。这些参数会在excel的关键字行里被搜索并填入
+    ParmDict = {"上机数":0,"接班模数":0,"交班模数":0,"每模数":0}
+
+    #这里定义输出excel文件
+    FileName = "c:\Documents and Settings\Administrator\桌面\Book1.xls"
+
+    #这里定义输出到excel文件第几个sheet(第一页为0)
+    OutFileSheetNumber = 0
+    
+    #这里定义关键字行是哪一行(excel中的第二行是 1)
+    OutFileKeyRowNumber = 1
+        
     def __init__(self):
         #do some init..
         pass
@@ -21,17 +42,21 @@ class RecordClient():
 
     def getUserName(self,id):
         #ToDo: exception
-        return self.UserDict[id];
+        self.username = self.UserDict[id];
+        return self.username 
 
     def calculateShift(self):
+        #这里定义如何计算班次
         #每天3班，8：00-16：00，16：00-24：00，24：00-8：00
         hour = datetime.datetime.now().hour
-        if hour in range(9,17):
-            return self.ShiftDict['Morning']
-        if hour in range(17,23):
-            return self.ShiftDict['Middle']
-        if hour in range(0,9):
-            return self.ShiftDict['Night']
+        if hour in range(9,17): #      9 <= hour <17
+            self.shift = self.ShiftDict['Morning']
+        if hour in range(17,24):#      17 <= hour <24
+            self.shift =  self.ShiftDict['Middle']
+        if hour in range(0,9):  #      0 <= hour <8
+            self.shift =  self.ShiftDict['Night']
+
+        return self.shift
         
     def scanbarcode(self):
         #Todo: add some timeout
@@ -41,6 +66,8 @@ class RecordClient():
 
     def getMachineAndProduct(self):
         (M,P) = self.MPdict[self.scanbarcode()]
+        self.Machine = M
+        self.Product = P
         return M,P
 
     def queryParms(self):
@@ -50,13 +77,37 @@ class RecordClient():
 
     def updateDB(self):
             print "正在写入数据库.....\n"
+            rb = open_workbook(self.FileName)
+            sheet = rb.sheets()[self.OutFileSheetNumber]
+            wb = copy(rb)
+            row = wb.get_sheet(self.OutFileSheetNumber).row(rb.sheets()[self.OutFileSheetNumber].nrows)
+            row.write(3,encode(self.username))
+            row.write(2,encode(self.userid))
+            row.write(4,encode(self.Product))
+            row.write(5,encode(self.Machine))
+            row.write(6,encode(self.shift))
+            for parm in self.ParmDict:
+                for col in range(sheet.ncols):
+                    cellValue = sheet.cell(self.OutFileKeyRowNumber,col).value
+                    print cellValue
+                    print unicode(parm,'cp936')
+                    if cellValue == unicode(parm,'cp936'):
+                        print "found "+parm+"at"+str(col)
+                        row.write(col,encode(self.ParmDict[parm]))
+
+            #保存到文件
+            wb.save(self.FileName)
             print "写入数据库完成！\n"
  
     def printViaPrinter(self):
             print "正在打印\n"
             print "打印完成请取票\n"
 
-if __name__ == '__main__':
+            
+def encode(str):
+    return unicode(str,'cp936')
+
+def mainloop():
     while True :
         try:
             is_name_confirmed = False
@@ -84,7 +135,7 @@ if __name__ == '__main__':
                 this.queryParms()
                 print "=====请确认以下信息,你这次输入的是====="
                 for each in this.ParmDict:
-                    print each;
+                    print each+":\t"+this.ParmDict[each];
                 if raw_input("(正确=y)：" + UserName+"\n") == "y":
                     is_Parms_confirmed = True
             this.updateDB()
@@ -98,8 +149,8 @@ if __name__ == '__main__':
             print excep1.args
             
         
-    
-    
+if __name__ == '__main__':    
+    mainloop()
     
     
     
